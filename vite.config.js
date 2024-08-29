092b35f50e854a65faaf641f7955fbd8caded93f
Vib-D <vibhordass2@gmail.com>
2024-08-29 18:27:19 +0530

    config changes

--- vite.config.js
+++ vite.config.js
@@ -8,19 +8,6 @@ export default defineConfig({
   base: '/PORTFOLIO/',
   build: {
     outDir: 'dist',
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return id
-              .toString()
-              .split('node_modules/')[1]
-              .split('/')[0]
-              .toString();
-          }
-        },
-      },
-    },
     chunkSizeWarningLimit: 1000, // Increase the limit to avoid warnings
   },
 })
